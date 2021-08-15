44808f24fe3d4d7bd30a5e88ee1e85bdf266669c
okosival <valentine11.dev@gmail.com>
2021-08-15 15:04:04 +0100

    fixed bug in importation of store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import configureStore from "./redux/configureStore";
+import { store } from "./redux/configureStore";
 
-const store = configureStore();
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
