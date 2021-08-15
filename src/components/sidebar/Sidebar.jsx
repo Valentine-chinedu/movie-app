0da3570a624f97acc86aec1eb76581d2edccb7df
okosival <valentine11.dev@gmail.com>
2021-08-15 17:24:19 +0100

    created sidebar component and css

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.scss";
+import { AiFillPlayCircle } from "react-icons/ai";
+
+import { genres } from "../../helpers/genres";
+
+const Sidebar = ({ genreName }) => {
+	const renderGenres = () => {
+		return genres.map((genre) => {
+			return (
+				<Link
+					to={`/movies/${genre.name}`}
+					key={genre.id}
+					className={`sidebar__inside ${
+						genre.name === genreName ? "active" : ""
+					}`}
+				>
+					<AiFillPlayCircle className='sidebar__inside--icon' />
+					{genre.name}
+				</Link>
+			);
+		});
+	};
+	return (
+		<div className='sidebar'>
+			<Link to='/' className='sidebar__logo'>
+				<img src='/logo.png' alt='logo-pic' />
+			</Link>
+			<h3 className='sidebar__heading'>Genres</h3>
+			{renderGenres()}
+		</div>
+	);
+};
+
+export default Sidebar;
