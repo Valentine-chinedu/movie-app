94a2cc928bc0702dace6326b36d7d02c6370f2a8
okosival <valentine11.dev@gmail.com>
2021-08-15 17:35:39 +0100

    created reducers

---
+++ src/redux/reducers/castReducers.js
@@ -0,0 +1,23 @@
+const castListReducer = (state = { casts: [] }, action) => {
+	switch (action.type) {
+		case "FETCH_CAST_LIST_REQUEST":
+			return { ...state, loading: true };
+		case "FETCH_CAST_LIST_SUCCESS":
+			return { ...state, loading: false, casts: action.payload };
+		default:
+			return state;
+	}
+};
+
+const castDetailsReduce = (state = { cast: {} }, action) => {
+	switch (action.type) {
+		case "FETCH_CAST_DETAILS_REQUEST":
+			return { ...state, loading: true };
+		case "FETCH_CAST_DETAILS_SUCCESS":
+			return { ...state, loading: false, cast: action.payload };
+		default:
+			return state;
+	}
+};
+
+export { castListReducer, castDetailsReduce };
--- src/redux/reducers/moviesReducer.js
+++ src/redux/reducers/moviesReducer.js
@@ -1,18 +1,36 @@
-import { FETCH_MOVIES } from "../actions/types";
+const movieListReducer = (state = { movies: [] }, action) => {
+	switch (action.type) {
+		case "FETCH_MOVIE_LIST_REQUEST":
+			return { ...state, loading: true };
+		case "FETCH_MOVIE_LIST_SUCCESS":
+			return { ...state, loading: false, movies: action.payload };
 
-const movies = (state = [], action) => {
-	if (action.type === FETCH_MOVIES) {
-		if (action.payload.page === 1) {
-			return action.payload;
-		}
-
-		return {
-			page: action.payload.page,
-			results: [...state.results, ...action.payload.results],
-		};
+		default:
+			return state;
 	}
-
-	return state;
 };
 
-export default movies;
+const movieDetailsReducer = (state = { movie: { genres: [] } }, action) => {
+	switch (action.type) {
+		case "FETCH_MOVIE_DETAILS_REQUEST":
+			return { ...state, loading: true };
+		case "FETCH_MOVIE_DETAILS_SUCCESS":
+			return { ...state, loading: false, movie: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+const movieVideoReducer = (state = { videos: [{ key: "" }] }, action) => {
+	switch (action.type) {
+		case "FETCH_MOVIE_VIDEO_REQUEST":
+			return { ...state, loading: true };
+		case "FETCH_MOVIE_VIDEO_SUCCESS":
+			return { ...state, loading: false, videos: action.payload };
+		default:
+			return state;
+	}
+};
+
+export { movieListReducer, movieDetailsReducer, movieVideoReducer };
