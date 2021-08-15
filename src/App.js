5bd2243e4c082a53a853941650ee48ea94e0ace4
okosival <valentine11.dev@gmail.com>
2021-08-15 17:39:50 +0100

    completed app routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-	return <div></div>;
-}
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./helpers/scrollToTop";
+import CastScreen from "./pages/CastScreen";
+import FilteredListScreen from "./pages/FilteredListScreen";
+import HomeScreen from "./pages/HomeScreen";
+import MovieScreen from "./pages/MovieScreen";
 
+const App = () => {
+	return (
+		<Router>
+			<ScrollToTop />
+			<Route path='/' exact component={HomeScreen} />
+			<Route path='/movies/:genre' component={HomeScreen} />
+			<Route path='/movie/:id' component={MovieScreen} />
+			<Route path='/cast/:id' component={CastScreen} />
+			<Route path='/:filteredKeyword' exact component={FilteredListScreen} />
+		</Router>
+	);
+};
 export default App;
