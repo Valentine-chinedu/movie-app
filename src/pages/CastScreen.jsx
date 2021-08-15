a1f1404d676577642278a25f363e6f49801ab981
okosival <valentine11.dev@gmail.com>
2021-08-15 17:54:55 +0100

    fixed import error

--- src/pages/CastScreen.jsx
+++ src/pages/CastScreen.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import CastDetails from "../components/CastDetails/CastDetails";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
-import Sidebar from "../components/Sidebar/Sidebar";
+import CastDetails from "../components/castdetails/CastDetails";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const CastScreen = ({ match }) => {
 	return (
