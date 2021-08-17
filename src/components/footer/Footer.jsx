58a074bf063d0ece4d7af0d3d461fb054467e265
okosival <valentine11.dev@gmail.com>
2021-08-17 16:37:10 +0100

    added logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiFillGithub } from "react-icons/ai";
+
 import "./footer.scss";
 
 const Footer = () => {
@@ -8,32 +8,17 @@ const Footer = () => {
 			<div className='f-container'>
 				<div className='f-container__img-wrapper mb-small'>
 					<img
+						loading='lazy'
 						className='f-container__logo'
-						src='/tmdb-logo.svg'
+						src='/logo.png'
 						alt='logo-pic'
 					/>
-					<a
-						className='f-container__source'
-						rel='noreferrer'
-						target='_blank'
-						href='https://github.com/pratyushranjanpadhi/moviescout'
-					>
-						<span>Source</span>
-						<AiFillGithub className='f-container__git' />{" "}
-					</a>
 				</div>
-				<h3>
-					Designed and Developed by{" "}
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://github.com/pratyushranjanpadhi'
-					>
-						Pratyush
-					</a>
-				</h3>
-				<h3>Copyright &copy; movieScout 2021 | All Rights Reserved </h3>
+
+				<h3>Copyright &copy; coolmovies 2021 | All Rights Reserved </h3>
 			</div>
 		</div>
 	);
 };
+
+export default Footer;
